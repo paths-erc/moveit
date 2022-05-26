d92523aae66b1d842c34e78e1ee654fcd107f22b
Julian Bogdani <jbogdani@gmail.com>
2022-05-26 10:27:12 +0200

    Extracted typeWeight

--- src/convertGeoJSON.js
+++ src/convertGeoJSON.js
@@ -1,24 +1,17 @@
-const fs = require('fs');
+import * as fs from 'fs';
+import typeWeight from './typeWeights.js';
 
 const rawdata = fs.readFileSync('./src/geojson/arcs.geojson');
 
 const geojson = JSON.parse(rawdata);
 
-const typeWeights = {
-  'coast': 1,
-  'channel': 1.5,
-  'river': 1.5,
-  'sea': 1.5,
-  'desert': 2
-};
-
 geojson.features.forEach( (f, i) => {
   geojson.features[i].properties = {
     "fid": geojson.features[i].properties.fid,
-    "typeWeight": typeWeights[geojson.features[i].properties.type] || 1
+    "typeWeight": typeWeight[geojson.features[i].properties.type] || 1
   };
 });
 
 fs.writeFileSync('src/geojson/arcs.json', JSON.stringify(geojson));
 
-module.exports = geojson;
+export default geojson;
---
+++ src/typeWeights.js
@@ -0,0 +1,9 @@
+const typeWeights = {
+  'river': .8,
+  'channel': .8,
+  'sea': 1,
+  'coast': 1,
+  'desert': 2
+};
+
+export default typeWeights;
